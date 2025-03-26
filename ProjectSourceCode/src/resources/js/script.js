334fffa396ac55e0a3aba1c463d395bfc6eae750
leah <leahtiktin@gmail.com>
2025-03-25 20:33:43 -0600

    modal closes when x is clicked

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,11 +1,20 @@
+//refactor this
+
 const infoBtns = document.querySelectorAll(".infoBtn");
 infoBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const modals = document.querySelectorAll(".infoModal");
     const modal = document.querySelector(
       `.infoModal[index = '${btn.getAttribute("index")}']`
     );
-    console.log(modals);
+
     $(modal).modal("show");
+  });
+});
+
+const modals = document.querySelectorAll(".infoModal");
+modals.forEach((modal) => {
+  const closeButton = modal.querySelector(".closeInfo");
+  closeButton.addEventListener("click", () => {
+    $(modal).modal("hide");
   });
 });
