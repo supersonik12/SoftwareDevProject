96ee77d105ac0f25c7c12b70cc5e9e17b2f5a0c1
supersonik12 <smf1238@outlook.com>
2025-04-10 09:46:42 -0600

    Added unit tests for quiz

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -8,23 +8,23 @@ const chai = require('chai'); // Chai HTTP provides an interface for live integr
 const chaiHttp = require('chai-http');
 chai.should();
 chai.use(chaiHttp);
-const {assert, expect} = chai;
+const { assert, expect } = chai;
 
 // ********************** DEFAULT WELCOME TESTCASE ****************************
 
 describe('Server!', () => {
-  // Sample test case given to test / endpoint.
-  it('Returns the default welcome message', done => {
-    chai
-      .request(server)
-      .get('/welcome')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body.status).to.equals('success');
-        assert.strictEqual(res.body.message, 'Welcome!');
-        done();
-      });
-  });
+	// Sample test case given to test / endpoint.
+	it('Returns the default welcome message', done => {
+		chai
+			.request(server)
+			.get('/welcome')
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body.status).to.equals('success');
+				assert.strictEqual(res.body.message, 'Welcome!');
+				done();
+			});
+	});
 });
 
 describe('Test GET method for login page', () =>
@@ -43,11 +43,11 @@ describe('Test POST method for successful sign in', () =>
 		chai
 			.request(server)
 			.get('/login')
-			.send({email: 'test@example.com', password: 'testpassword'})
+			.send({ email: 'test@example.com', password: 'testpassword' })
 			.end((err, res) => {
 				expect(res.body.message.to.equals('Success'));
 			});
-			done();
+		done();
 	})
 );
 
@@ -56,7 +56,7 @@ describe('Test POST method for failed sign in', () =>
 		chai
 			.request(server)
 			.get('/login')
-			.send({email: 'invalid@example.com', password: 'invalidpassword'})
+			.send({ email: 'invalid@example.com', password: 'invalidpassword' })
 			.end((err, res) => {
 				expect(res.body.message.to.equals('Invalid username or password'));
 			});
@@ -65,12 +65,12 @@ describe('Test POST method for failed sign in', () =>
 );
 
 
-describe('Test POST method for successful register', () => 
+describe('Test POST method for successful register', () =>
 	it('Redirects to the quiz', done => {
 		chai
 			.request(server)
 			.get('/request')
-			.send({email: 'newuser@example.com', password: 'abc123', name: 'John Doe'})
+			.send({ email: 'newuser@example.com', password: 'abc123', name: 'John Doe' })
 			.end((err, res) => {
 				expect(res.body.message.to.equals('Success'));
 			});
@@ -83,7 +83,7 @@ describe('Test POST method for unsuccessful register', () =>
 		chai
 			.request(server)
 			.get('/request')
-			.send({email: 123, password: 'abc123', name: 'John Doe'})
+			.send({ email: 123, password: 'abc123', name: 'John Doe' })
 			.end((end, res) => {
 				expect(res.body.message.equals('Not a valid email'));
 			});
@@ -92,4 +92,58 @@ describe('Test POST method for unsuccessful register', () =>
 );
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
+describe('GET /purrsonality-quiz', () => {
+	it('Renders quiz page', (done) => {
+		chai
+			.request(app)
+			.get('/purrsonality-quiz')
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				done();
+			});
+	});
+});
+
+describe('POST /purrsonality-quiz', () => {
+	it('Test valid input (redirects to home page)', (done) => {
+		chai
+			.request(app)
+			.post('/purrsonality-quiz')
+			.send({
+				species: 'dog',
+				aff_val: 0.5,
+				open_val: 0.3,
+				play_val: 0.7,
+				vigilant_val: 0.2,
+				train_val: 0.8,
+				energy_val: 0.6,
+				bored_val: 0.4,
+			})
+			.end((err, res) => {
+				expect(res).to.redirect;
+				done();
+			});
+	});
+
+	it('Test invalid input', (done) => {
+		chai
+			.request(app)
+			.post('/purrsonality-quiz')
+			.send({
+				species: 'dog',
+				aff_val: 2,
+				open_val: 0.5,
+				play_val: 0.5,
+				vigilant_val: 0.5,
+				train_val: 0.5,
+				energy_val: 0.5,
+				bored_val: 0.5,
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(423);
+				expect(res.body).to.have.property('error', 'Values outside expected range');
+				done();
+			});
+	});
+});
 // ********************************************************************************
